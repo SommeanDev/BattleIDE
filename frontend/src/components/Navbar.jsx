cb2642161118cdaa7c4c87e74bc5725d6e7560a0
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-08-29 23:56:02 +0530

    add home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,45 +1,20 @@
-
+import logo from "../assets/BattleIDE.png"
 
 const Navbar = () => {
   return (
-    <header className="flex items-center justify-between border-b border-b-[#233c48] px-10 py-2 bg-[#111c22]">
-      <div className="flex items-center gap-4 text-white">
-        <div className="size-4">
-          <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M24 18.4228L42 11.475V34.3663C42 34.7796 41.7457 35.1504 41.3601 35.2992L24 42V18.4228Z"
-              fill="currentColor"
-            />
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M24 8.18819L33.4123 11.574L24 15.2071L14.5877 11.574L24 8.18819ZM9 15.8487L21 20.4805V37.6263L9 32.9945V15.8487ZM27 37.6263V20.4805L39 15.8487V32.9945L27 37.6263ZM25.354 2.29885C24.4788 1.98402 23.5212 1.98402 22.646 2.29885L4.98454 8.65208C3.7939 9.08038 3 10.2097 3 11.475V34.3663C3 36.0196 4.01719 37.5026 5.55962 38.098L22.9197 44.7987C23.6149 45.0671 24.3851 45.0671 25.0803 44.7987L42.4404 38.098C43.9828 37.5026 45 36.0196 45 34.3663V11.475C45 10.2097 44.2061 9.08038 43.0155 8.65208L25.354 2.29885Z"
-              fill="currentColor"
-            />
-          </svg>
-        </div>
-        <h2 className="text-white text-lg font-bold tracking-tight">BattleIDE</h2>
-      </div>
-
-      <nav className="flex flex-1 justify-end gap-8 items-center">
-        <div className="flex gap-9 text-white text-sm font-medium">
-          <a href="#">Home</a>
-          <a href="#">Challenges</a>
-          <a href="#">Learn</a>
-          <a href="#">Community</a>
-          <a href="#">Compete</a>
-        </div>
-        <div className="flex gap-2">
-          <button className="h-10 px-4 rounded-xl bg-[#2badee] text-[#111c22] text-sm font-bold">
-            Get Started
+    <header className="flex w-full justify-between fixed bg-transparent z-50 p-4   ">
+      <img src={logo} alt="logo" className="h-8" />
+      <div className="text-white">
+        <div class="text-lg bg-gradient-to-r flex  gap-3   from-black/60 to-white/5   backdrop-blur-lg text-white  p-2 px-4    rounded-full border-2 border-l-0 border-b-1 border-white/30 filter ">
+          <button className="hover:text-cyan-500 cursor-pointer">
+            Register
           </button>
-          <button className="h-10 px-4 rounded-xl bg-[#233c48] text-white text-sm font-bold">
+          <span  className="select-none">|</span>
+          <button className="hover:text-cyan-500 cursor-pointer">
             Login
           </button>
         </div>
-      </nav>
+      </div>
     </header>
   );
 };
