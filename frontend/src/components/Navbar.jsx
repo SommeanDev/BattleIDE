9ac9897b87a1486c6d08c4a9e82ad547421c853e
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-09-06 01:13:49 +0530

    connect MongoDB

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-reac
 import logo from "../assets/BattleIDE.png"
 
 const Navbar = () => {
+
   return (
     <header className="flex w-full justify-between fixed bg-transparent z-50 p-4">
       <img src={logo} alt="logo" className="h-8" />
@@ -24,7 +25,7 @@ const Navbar = () => {
       </SignedOut>
       <SignedIn>
         <div className="flex gap-2">
-          <div className="bg-gradient-to-r from-black/60 to-white/5 text-white font-semibold backdrop-blur-lg flex justify-center items-center px-4 rounded-4xl border-2 border-l-0 border-b-1 border-white/30 filter">
+          <div className="bg-gradient-to-r select-none from-black/60 to-white/5 text-white font-semibold backdrop-blur-lg flex justify-center items-center px-4 rounded-4xl border-2 border-l-0 border-b-1 border-white/30 filter">
             ACE I
           </div>
           <div className="bg-gradient-to-r from-black/60 to-white/5 backdrop-blur-lg flex justify-center items-center p-1 rounded-full border-2 border-l-0 border-b-1 border-white/30 filter">
