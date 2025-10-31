3062bbcc0877a1804a986700b0da9e6a00e33c0f
Prajyot Gorlewar <prajyotgorlewar@gmail.com>
2025-10-31 22:14:03 +0530

    chore: add a blank line for better readability in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router-dom'; // 👈 Import RouterProvider
 import router from './routes/route.jsx';
 import { ClerkProvider } from '@clerk/clerk-react';
 
+
 // Your Clerk Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
