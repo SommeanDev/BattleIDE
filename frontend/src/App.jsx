2a737a19420a4bae7e850fefeabd90d9a5f663b0
Ketan Pardhi <ketanpardhi7@gmail.com>
2025-10-31 22:16:37 +0530

    some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 // src/App.jsx - CORRECTED
 import React from 'react';
 import Navbar from './components/Navbar';
-import { Outlet } from 'react-router-dom'; // 👈 1. Import Outlet
-import React from "react";
+import { Outlet } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Battle from "./pages/battle";
 
