479a0f7d19b95cb4426353adaaa5c80ea36e79c0
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-11-03 10:08:57 +0530

    add frontend url

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const app = express();
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",process.env.FRONTEND_URL],
     credentials: true, 
   })
 );
